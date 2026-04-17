08c80a1098eec25a93ec84fe435dfa2a5a6df692
Štěpán Marek <stepina.mark@seznam.cz>
2026-04-17 19:29:07 +0200

    Update Flutter Web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1232303290" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2677501940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
